e485dbd5c9b10d3f371c84e984cf5a67779437c9
Miguel Chaparro <miguely.chaparros@utadeo.edu.co>
2017-10-21 14:13:00 -0500

    Add variable Double in DataTypeSwift.swift

--- DataTypeSwift.swift
+++ DataTypeSwift.swift
@@ -7,3 +7,7 @@ let binario = 0b10001 // 17 en notación binaria
 let octal = 0o21 // 17 en notación octal
 let hexadecimal = 0x11 // 17 en notación hexadecimal
 let suma = decimal + octal // #=> 34
+//   Double
+let    decimal   =    12.1875
+let    exponente   =    1.21875e1
+let    hexadecimal   =    0xC.3p0
