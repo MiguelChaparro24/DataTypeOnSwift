b76a37c848648b9f177cb857ab60490e4c800b75
Andres Merchan <yeissona.merchanl@utadeo.edu.co>
2017-10-21 14:04:38 -0500

    Edit file DataTypeSwift.swift

--- DataTypeSwift.swift
+++ DataTypeSwift.swift
@@ -2,3 +2,8 @@
 let    dedosEnLaMano   =    5
 let    autosEnLaCochera   =    2
 let    estrellasEnElCielo   =    1_000_000     //   Puedes   usar   _   entre   números para   mejorar   la   legibilidad
+let decimal = 17
+let binario = 0b10001 // 17 en notación binaria
+let octal = 0o21 // 17 en notación octal
+let hexadecimal = 0x11 // 17 en notación hexadecimal
+let suma = decimal + octal // #=> 34
